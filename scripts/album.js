2fb6b515ccbc650617820cd6957f17fd0cd589e6
Thomas Kurth <thomasbk86@gmail.com>
2017-08-10 13:45:30 -0400

    assignment-26

--- scripts/album.js
+++ scripts/album.js
@@ -78,14 +78,20 @@ var setCurrentAlbum = function(album) {
  };
 
  var findParentByClassName = function(element, targetClass) {
-     if (element) {
-         var currentParent = element.parentElement;
-         while (currentParent.className !== targetClass && currentParent.className !== null) {
-             currentParent = currentParent.parentElement;
-         }
-         return currentParent;
-     }
- };
+      if (element) {
+          var currentParent = element.parentElement;
+          if (currentParent === null) {
+            console.log("No parent found.");
+      }
+          else if (targetClass === null) {
+            console.log("No parent found with that class name.");
+          }
+          while (currentParent.className !== targetClass && currentParent.className !== null) {
+              currentParent = currentParent.parentElement;
+          }
+          return currentParent;
+      }
+  };
 
  var getSongItem = function(element) {
     switch (element.className) {
