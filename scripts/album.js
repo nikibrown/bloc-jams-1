b91f94071e0f6fa0cb919af6bf8756e07db598fe
Thomas Kurth <thomasbk86@gmail.com>
2017-08-14 15:19:30 -0400

    checkpoint-28

--- scripts/album.js
+++ scripts/album.js
@@ -78,20 +78,22 @@ var setCurrentAlbum = function(album) {
  };
 
  var findParentByClassName = function(element, targetClass) {
-      if (element) {
-          var currentParent = element.parentElement;
-          if (currentParent === null) {
-            console.log("No parent found.");
-      }
-          else if (targetClass === null) {
-            console.log("No parent found with that class name.");
-          }
-          while (currentParent.className !== targetClass && currentParent.className !== null) {
-              currentParent = currentParent.parentElement;
-          }
-          return currentParent;
-      }
-  };
+     var currentParent = element.parentElement;
+
+     if (currentParent) {
+         while (currentParent.className && currentParent.className != targetClass) {
+             currentParent = currentParent.parentElement;
+         }
+
+         if (currentParent.className == targetClass) {
+             return currentParent;
+         } else {
+             alert("No parent with that class name found.");
+         }
+     } else {
+         alert("No parent found.");
+     }
+ };
 
  var getSongItem = function(element) {
     switch (element.className) {
